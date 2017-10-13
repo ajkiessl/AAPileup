8b5a2e469f25f51f17a73e26d7fec402f65b297f
Alex Kiessling <ajkiessl@gmail.com>
2017-10-13 17:53:58 -0400

    NGSsim.py, alignvariants.sh added README.md updated

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AAPileup
-Bioinformatics tool for taking NGS and converting it to amino acid pileup
+Bioinformatics tool for taking NGS and converting it to amino acid pileup.
+NGSsim.py can simulate next-generation sequence output from a Sanger output.
+Must download and install samtools and bowtie2.  They must be in $PATH for this pipeline to work.
---
+++ alignvariants.sh
@@ -0,0 +1,26 @@
+"""
+This bash script runs a pipeline to convert a fastq file to BAM pileup format.
+It takes a fastq and fasta file as inputs.
+Bowtie2 is the aligner used.
+"""
+#!/bin/bash
+fastqfile=$1
+fastafile=$2
+echo $fastafile, $fastqfile
+fastabase=$( basename $fastafile )
+fastabaseid=${fastabase%.*}
+filteredfastqfile=$( basename $fastqfile )
+filteredfastqfilename=${filteredfastqfile%.*}_filtered.fastq
+python fastqfilter.py -fastqinput $fastqfile -outputid $filteredfastqfilename
+samoutfile=$( basename $filteredfastqfile )
+samoutfilename=${samoutfile%.*}.sam
+bowtie2-build $fastafile $fastabaseid
+bowtie2 -x $fastabaseid -q $filteredfastqfilename -D 60 -R 6 -N 1 -L 20 -i S,1,0.50 --reorder -S $samoutfilename
+samtobamfile=$( basename $samoutfilename )
+samtobamfilename=${samtobamfile%.*}.bam
+samtools view -bS $samoutfilename > $samtobamfilename
+sortedbamfile=$( basename $samtobamfilename )
+sortedbamfilename=${sortedbamfile%.*}_sorted
+samtools sort $samtobamfilename $sortedbamfilename
+indexinput=$( basename $sortedbamfilename ).bam
+samtools index $indexinput
